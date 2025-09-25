34eae19584400bf0cf85d1fcf63b2197b82c5d59
José Ferro <101825159+zedawhite@users.noreply.github.com>
2025-09-25 12:52:45 +0100

    Update script.js

--- 06_06/script.js
+++ 06_06/script.js
@@ -5,14 +5,14 @@
 
 // String:
 let stringDemo = "A string of text.";
-console.log("String:", stringDemo);
+console.log("String:", typeof stringDemo);
 
 // Numbes:
 let integerDemo = 4;
 console.log("Integer:", integerDemo);
 
 let floatDemo = 5.6;
-console.log("Floating point number:", floatDemo);
+console.log("Floating point number:", typeof floatDemo);
 
 // Boolean:
 let booleanDemo = true;
