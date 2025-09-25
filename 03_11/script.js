4348e23d0271af69d4eae209f5d302f67db605ab
José Ferro <101825159+zedawhite@users.noreply.github.com>
2025-09-25 11:14:14 +0100

    Update script.js

--- 03_11/script.js
+++ 03_11/script.js
@@ -39,3 +39,45 @@ const everydayPack = new Backpack(
   26,
   false
 );
+
+class HikingBackpack extends Backpack {
+  constructor(
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen,
+    hydrationCapacity
+  ) {
+    // Initialize the parent class properties
+    super(name, volume, color, pocketNum, strapLengthL, strapLengthR, lidOpen);
+    // New property specific to HikingBackpack
+    this.hydrationCapacity = hydrationCapacity; // Capacity in liters
+  }
+
+  // Method to check the hydration level and alert if it needs refilling
+  checkHydration() {
+    if (this.hydrationCapacity > 0) {
+      console.log(`You have ${this.hydrationCapacity} liters of water left.`);
+    } else {
+      console.log("Time to refill your water!");
+    }
+  }
+
+  // Extend or override methods from the parent class if necessary
+  // For example, adding extra functionality when the lid is toggled
+  toggleLid(lidStatus) {
+    super.toggleLid(lidStatus); // Call the parent method
+    if (lidStatus) {
+      console.log(
+        "Your hiking backpack lid is open. Remember to check to make sure the hydration pack is inserted."
+      );
+    } else {
+      console.log(
+        "Your hiking backpack lid is closed. Remember to check to make sure the hydration pack is inserted."
+      );
+    }
+  }
+}
