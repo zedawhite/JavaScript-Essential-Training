7c3a187857c2bf074b9f6253fa0f380c35457bfe
José Ferro <101825159+zedawhite@users.noreply.github.com>
2025-09-25 12:46:23 +0100

    Update script.js

--- 06_03/script.js
+++ 06_03/script.js
@@ -8,12 +8,14 @@ var color = "purple";
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
 
+color = "skyblue";
+
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
 
-// function headingColor() {
-//   color = "blue";
-//   document.querySelector(".title").style.color = color;
-// }
+function headingColor() {
+  color = "blue";
+  document.querySelector(".title").style.color = color;
+}
 
-// headingColor();
+headingColor();
