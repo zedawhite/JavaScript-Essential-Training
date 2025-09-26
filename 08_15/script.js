eb0ceefdb7933be09f4d9458cb88800a773b0d33
José Ferro <101825159+zedawhite@users.noreply.github.com>
2025-09-26 13:11:06 +0100

    Update script.js

--- 08_15/script.js
+++ 08_15/script.js
@@ -9,10 +9,18 @@ const article = document.querySelector("article");
 let stuffList = document.createElement("ul");
 
 // forEach() array method
-stuff.forEach((item) => {
+const stuffItems = stuff.map((item) => {
   let listItem = document.createElement("li");
   listItem.innerHTML = item;
-  stuffList.append(listItem);
+  return listItem;
+  // stuffList.append(listItem);
 });
+
+stuffItems.forEach(item) => {
+  stuffList.append(item);
+}
+
+console.log("stuff:", stuff)
+console.log("stuffItems:", stuffItems)
 
 article.append(stuffList)
