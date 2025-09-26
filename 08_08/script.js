84eb998a713ac9937426794cedf7fea5f03496a6
José Ferro <101825159+zedawhite@users.noreply.github.com>
2025-09-26 12:46:29 +0100

    Update script.js

--- 08_08/script.js
+++ 08_08/script.js
@@ -8,23 +8,23 @@
  * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
-const formatter = (value) => {
-  let formattedValue = new new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(
+const formatter = (locale, currency, value) => {
+  let formattedValue = new new Intl.NumberFormat(locale, { style: "currency", currency: currency }).format(
     value,
   ),
 );
   return formattedValue
 }
 
-const tipCalculator = (sum, percentage) => {
+const tipCalculator = (sum, percentage, locale, currency) => {
   let tip = sum * (percentage / 100);
   let total = sum + tip;
   console.log(`
-      Sum before tip: ${formatter(sum)}
+      Sum before tip: ${formatter(locale, currency, sum)}
       Tip percentage: ${percentage}%
-      Tip:            ${formatter(tip)}
-      Total:          ${formatter(total)}
+      Tip:            ${formatter(locale, currency, tip)}
+      Total:          ${formatter(locale, currency, total)}
     `);
 };
 
-tipCalculator(29.95, 18);
+tipCalculator(29.95, 18, "de-DE", "EUR");
